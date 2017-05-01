101b4e5fb29195e500103230b3bbdae2369fde75
QasimK <noreply@iamqasimk.com>
2017-05-01 17:14:09 +0100

    Fix failing tests (version number)

--- tests/test_mal_scraper.py
+++ tests/test_mal_scraper.py
@@ -4,7 +4,7 @@ import mal_scraper
 def test_import_mal_scraper():
     """Can we import mal_scraper"""
     assert mal_scraper
-    assert mal_scraper.__version__.split('.') == ['0', '1', '0']
+    assert mal_scraper.__version__.split('.') == ['0', '2', '0']
 
 
 class TestAutomaticUserDicoveryIntegration(object):
